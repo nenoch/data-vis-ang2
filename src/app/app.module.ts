3e0899f2e988a4dfebc28807ce5bb3426464b47b
Irene Canuti <irene.canuti@gmail.com>
2017-07-03 15:26:38 +0100

    Add graph-area component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GraphAreaComponent } from './graph-area/graph-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphAreaComponent
   ],
   imports: [
     BrowserModule
