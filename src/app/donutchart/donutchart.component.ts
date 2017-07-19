007c30f7b46fe65b9738d12b9d8879acb6ae0e3d
BenJohnCarson <benjohncarson@gmail.com>
2017-07-19 16:07:23 +0100

    WIP

--- src/app/donutchart/donutchart.component.ts
+++ src/app/donutchart/donutchart.component.ts
@@ -1,15 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, HostListener} from '@angular/core';
+import { DataService } from '../shared/data.service';
+import { ErrorHandlerService } from '../error-handler/error-handler.service';
+import { ChartUtilsService } from '../shared/chart-utils.service';
+import { ISubscription } from 'rxjs/Subscription';
+
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-donutchart',
   templateUrl: './donutchart.component.html',
   styleUrls: ['./donutchart.component.css']
 })
-export class DonutchartComponent implements OnInit {
+export class DonutchartComponent implements OnInit, OnDestroy {
+  @ViewChild('donutchart') private donutContainer: ElementRef;
+  private data;
+  private xAxis;
+  private yAxis;
+  private margin = {top: 50, right: 20, bottom: 100, left: 45};
+  private width: number;
+  private height: number;
+  private aspectRatio = 0.7;
+  private colour = d3.scaleOrdinal(d3.schemeCategory20c);
+  private padAngle = 0.015;
+  private floatFormat = d3.format('.4r');
+  private cornerRadius = 3;
+  private percentFormat = d3.format(',.2%');
+  private subscription: ISubscription;
 
-  constructor() { }
+  @HostListener('window:resize', ['$event'])
+  onKeyUp(ev: UIEvent) {
+    if (this.dataExists()) {
+        this.createDonutchart();
+      }
+  }
+
+  constructor(private dataService: DataService, private errorService: ErrorHandlerService, private chartUtils: ChartUtilsService) {}
 
   ngOnInit() {
+    this.getData();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  private getData() {
+    this.subscription = this.dataService.dataStream.subscribe((data) => {
+      this.data = data;
+      if (this.dataExists()) {
+        this.setAxes();
+        this.createDonutchart();
+      }
+    });
+  }
+
+  private dataExists() {
+    return this.data.length !== 0;
+  }
+
+  private setAxes() {
+    const axes = [];
+    for (const k in this.data[0]) {
+      if (this.data[0].hasOwnProperty(k)) {
+      axes.push(k)
+      }
+    }
+    this.xAxis = axes[0];
+    this.yAxis = axes[1];
+  }
+
+  private setSize() {
+    const container = this.donutContainer.nativeElement;
+    this.width = container.offsetWidth - this.margin.left - this.margin.right;
+    this.height = this.aspectRatio * this.width - this.margin.top - this.margin.bottom;
+  }
+
+  private createDonutchart() {
+    this.resetBarchart();
+    this.setSize();
+
+    const element = this.donutContainer.nativeElement;
+
+
+  }
+
+  private resetBarchart() {
+    this.chartUtils.resetSVG();
   }
 
 }
