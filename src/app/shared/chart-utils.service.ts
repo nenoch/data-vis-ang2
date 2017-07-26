f65fa949a5c0797ba0c1d192b78ed7cc4b577afe
BenJohnCarson <benjohncarson@gmail.com>
2017-07-26 10:40:23 +0100

    Fixed bar errors

--- src/app/shared/chart-utils.service.ts
+++ src/app/shared/chart-utils.service.ts
@@ -1,15 +1,28 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { ErrorHandlerService } from '../error-handler/error-handler.service';
 import * as d3 from 'd3';
 
 
 @Injectable()
 export class ChartUtilsService {
 
-  constructor() { }
+  constructor(private errorService: ErrorHandlerService) { }
 
-  resetSVG(){
-    let svg = d3.select('svg');
+  public resetSVG() {
+    const svg = d3.select('svg');
     svg.remove();
+  }
+
+  public checkYAxis(data, yAxis) {
+    const error = { title: 'Y Axis Error', content: 'Please enter a numeric value for the Y Axis.'};
+    data.forEach(d => {
+      if (isNaN(d[yAxis])) {
+        this.errorService.handleError(error);
+        return false;
+      } else {
+        return true;
+      }
+    });
   }
 
 }
