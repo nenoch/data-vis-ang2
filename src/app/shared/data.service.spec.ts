1115b78eceffcb0f64afd975ef3741430beec851
Irene Canuti <irene.canuti@gmail.com>
2017-08-02 16:33:36 +0100

    WIP testing data.service

--- src/app/shared/data.service.spec.ts
+++ src/app/shared/data.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { HttpModule } from '@angular/http';
+import { AxisData } from '../graph-form/axis-data';
 import { DataService } from './data.service';
 
 describe('DataService', () => {
@@ -16,6 +17,21 @@ describe('DataService', () => {
   beforeEach(inject([DataService], s => {
   service = s;
   }));
+
+  it('should return columns', async(() => {
+    // add async
+    service.url = 'assets/data/main-data.csv';
+    service.getCSVColumns().subscribe(x => {
+      expect(x).toContain('Country');
+    });
+  }));
+
+  it('WIP should set the axes and generate data accordingly', () => {
+    let axes1 = new AxisData('Country', 'Life Expectancy');
+    service.url = 'assets/data/main-data.csv';
+    service.setAxes(axes1, 'barchart')
+    expect(service.axes).toEqual(axes1);
+  });
 
   it('should convert item to Number if string is number', () => {
     expect(service.isNumber('9')).toEqual(9);
